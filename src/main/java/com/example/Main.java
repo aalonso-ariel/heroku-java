e1e477d8eb7e07b9c14ac464a9533b3dc96cdd35
Ariel Alonso <alonso.ariel90@gmail.com>
2019-10-08 14:44:08 -0300

    deploy test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -25,6 +25,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -50,7 +61,23 @@ public class Main {
 
   @RequestMapping("/")
   String index() {
-    return "index";
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("https://api.sandbox.paypal.com/v1/oauth2/token");
+        
+        Form form = new Form();
+        form.param("grant_type", "client_credentials");
+        
+        Entity<Form> entity = Entity.entity(form,
+        MediaType.APPLICATION_FORM_URLENCODED_TYPE);
+        
+        Invocation.Builder builder = target.request();
+        String response = builder.header("Accept", "application/json")
+                                 .header("Accept-Language", "en_US")
+                                 .header("Content-Type", "application/x-www-form-urlencoded")
+                                 .header("Authorization", "Basic QWNXZVFCM1hEMkJOTmtScnliVThFZ0RRVXc1QWVSNi16RUhobF9MdGl5ZEp5WXM3eWdGdWtqcEx1Rmt1bUhQZllPNm92d0dlVXNpeGI1Rk06RUJRVWpCcFdTMzM1OFpUZ3VGZWJwODAxbGJTNVAyMm83V3luRUtNc0M3MnlkajFibi1ZWmJMZ0RHX3hBdDdfdWdwOHluZXpfUnRhaDY3Tmw=")
+                                 .post(entity, String.class);
+        
+        return response;
   }
 
   @RequestMapping("/db")
